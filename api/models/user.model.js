629d98a4cd7d82210fb6154f2a26175b1a8b833c
Jagannath8 <jagannath9611@gmail.com>
2023-10-12 02:00:19 +0530

    create user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    pasword: {
+    password: {
         type: String,
         required: true,
     },
